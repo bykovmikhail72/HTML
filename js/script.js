eee92d8e3fe0307f7aa13fee85c2e6fe60b4478f
Mikhail <bykovmikhail72@gmail.com>
2021-09-07 00:50:36 +0500

    hamburger

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const hamburger = document.querySelector('.hamburger'),
+      menu = document.querySelector('.topnav__list'),
+      list = document.querySelectorAll('.topnav__item');
+
+hamburger.addEventListener('click', () => {
+    if (hamburger.classList.contains('hamburger-active')) {
+        hamburger.classList.remove('hamburger-active');
+        menu.style.left = "-100%";
+    } else {
+        hamburger.classList.add('hamburger-active');
+        menu.style.left = "0";
+    }
+});
+console.log(list)
+list.forEach(item => {
+    item.addEventListener('click', () => {
+        hamburger.classList.remove('hamburger-active');
+        menu.style.left = "-100%";
+    });
+});
